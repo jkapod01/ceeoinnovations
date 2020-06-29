1e65a954f19184d24113760febc581e70eac5080
amoore449 <moore.m.allison@gmail.com>
2020-06-29 09:30:18 -0700

    hi

--- _posts/2020-06-24-paper-robots.markdown
+++ _posts/2020-06-24-paper-robots.markdown
@@ -2,63 +2,38 @@
 layout: posttest
 displayimg: web/img/horizontal_paperbots_car.jpeg
 ---
-<div class="pdf" markdown="1">
-# PaperBots Cutouts
-[Pull-up man Cutout](web/pf/SPIKE_Silly-Walks_Placemat.pdf)
-[Silly Walks Placemat](web/pf/Pull-Up-Man-Lesson-Plan.pdf)
-[Silly Walks Placemat](web/pf/SPIKE_Silly-Walks_Placemat.pdf)
-</div>
-
 <div class="sitetitle hjhj" markdown="1">
 # PaperBots
 </div>
 
-
 <div class="video-text-overlay" markdown="1">
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/KZRW25MjqMQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/KZRW25MjqMQ" frameborder="0"
+allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 ## PaperBots Robot
 # Rubber-band Car
 
-This is a rubberband car is made from a single sheet of paper, 4 styrofoam wheels, straws, some paperclips and a rubber band.
+This is a rubberband car is made from a single sheet of paper, 4 styrofoam
+wheels, straws, some paperclips and a rubber band.
 </div>
 
 
 <div class="largeheaderimg" markdown="1">
 
-![Link to my pic](web/img/headimagerobot.jpg)
+![Link to my pic](../../../web/img/headimagerobot.jpg)
 
 ## An Inexpensive Option for Engineering Education
 # Accessible Robotics
 
-The cost of the equipment is a barrier to robotics education for many students. To combat these financial barriers to STEM education, PaperBots only require paper and low cost hardware to build, so offers a low-cost approach to robotics.
-
+The cost of the equipment is a barrier to robotics education for many students.
+To combat these financial barriers to STEM education, PaperBots only require
+paper and low cost hardware to build, so offers a low-cost approach to robotics.
 </div>
+
 <div class="pdf" markdown="1">
-# PaperBots Lesson Plans
-[Hexabot Lesson Plan](web/pf/HexaBot-Lesson-Plan.pdf)
-[Pull Up Man Lesson Plan](web/pf/Pull-Up-Man-Lesson-Plan.pdf)
-[Rubber Band Car Lesson Plan](web/pf/Rubber-Band-Car-Lesson-Plan.pdf)
-[Rube Golberg Lesson Plan](web/pf/Rube-Golberg-Lesson-Plan.pdf)
-
-</div>
-
-
-<div class="video-text-overlay" markdown="1">
-
-<iframe width="560" height="315" src="https://www.youtube.com/embed/DYoK3FkgMoQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-## PaperBots Robot
-# Hexapod
-This is a hexabot made mainly from cardstock with a simple motor to drive the linkages.
-
-</div>
-
-
-
-<div class="free-write" markdown="1">
-Hello there
-## Welcome to the dream land
-Here we talk about robots
+# PaperBots Cutouts
+[Pull-up man Cutout](../../../web/pf/SPIKE_Silly-Walks_Placemat.pdf)
+[Silly Walks Placemat](../../../web/pf/Pull-Up-Man-Lesson-Plan.pdf)
+[Silly Walks Placemat](../../../web/pf/SPIKE_Silly-Walks_Placemat.pdf)
 </div>
--- web/js/oldjs/oldjs.js
+++ web/js/oldjs/oldjs.js
@@ -1,4 +1,97 @@
+<script type="text/javascript">
 
+     function copyDivPI(){
+          if($( "div" ).hasClass( "pdf")){
+          //Alt_CreateHeaderPI();
+          console.log("returned to main");
+     }
+ }
+
+     function Alt_CreateHeaderPI(){
+               var loopmydivsPI  = document.getElementById('mycontent').getElementsByClassName('pdf');
+               var j;
+               for(j=0;j < (loopmydivsPI.length); j++){
+                    var retainmePI  = document.getElementsByClassName('pdf')[j];
+                    //initialize values from markdown to ter to header
+                    var sectionheaderPI = CollectmyHeader(retainmePI);
+
+                    var mylinkarrayPI = retainmePI.getElementsByTagName('a');
+
+                    var largernewdivPI = CreatenewDiv("largerpdfdatasection", "largerpdfsampledatasection");
+
+                    var largerelementPI = document.getElementsByClassName("largerappendherePI")[0];
+
+                    var sectionpdfheader = largernewdivPI.getElementsByClassName('pdfsectiontitle')[0];
+
+                    sectionpdfheader.innerHTML = sectionheaderPI.innerHTML;
+                    largerelementPI.appendChild(largernewdivPI);
+
+                    Alt_CreatePdfGrid(j, mylinkarrayPI);
+               }
+
+     }
+     function Alt_CreatePdfGrid(j, mylinkarrayPI){
+          var i;
+          for(i=0;i < (mylinkarrayPI.length); i++){
+               //creating a new div with a class name that copies the innerhtml of another div
+               if (mylinkarrayPI.length%4==0){
+                    var newdivPI = CreatenewDiv("col-sm-3 col-md-3 col-lg-3 pdfdatasection", "pdfsampledatasection");
+               } else if(mylinkarrayPI.length%3==0){
+                    var newdivPI = CreatenewDiv("col-sm-4 col-md-4 col-lg-4 pdfdatasection", "pdfsampledatasection");
+               } else if(mylinkarrayPI.length%2==0){
+                    var newdivPI = CreatenewDiv("col-sm-6 col-md-6 col-lg-6 pdfdatasection", "pdfsampledatasection");
+               }else if(mylinkarrayPI.length==1){
+                    var newdivPI = CreatenewDiv("col-sm col-md col-lg pdfdatasection", "pdfsampledatasection");
+               } else{
+                    var newdivPI = CreatenewDiv("col-sm-4 col-md-4 col-lg-4 pdfdatasection", "pdfsampledatasection");
+               }
+               var elementPI = AppendNewDiv("appendherePI", newdivPI, j);
+
+               AddInnerHTMLElementtoNewDiv("pdfheader", 0, mylinkarrayPI[i], newdivPI);
+               Alt_AddHrefSRCtoNewDiv("mypdf", 0, mylinkarrayPI[i], newdivPI);
+               Alt_AddHrefSRCtoNewDiv("click-to-download", 0, mylinkarrayPI[i], newdivPI);
+
+          }
+     }
+     function Alt_AddHrefSRCtoNewDiv(get_class, at_place, copy_this_data, search_here){
+          var copyhere = search_here.getElementsByClassName(get_class)[at_place];
+          copyhere.src = copy_this_data.href;
+          return copyhere;
+     }
+
+     function Alt_CollectallH1(section_to_check){
+          var allh1 = section_to_check.getElementsByTagName('h1');
+          var allh1 = Alt_EmptyHeaderCheck(allh1,section_to_check);
+
+          return allh1;
+     }
+
+     function CollectmyHeader(section_to_check){
+          var sectionheaderPI = section_to_check.getElementsByTagName('h1')[0];
+          var sectionheaderPI = Alt_yHeaderCheck(sectionheaderPI,section_to_check,"h2");
+          var sectionheaderPI = Alt_yHeaderCheck(sectionheaderPI,section_to_check,"h3");
+          var sectionheaderPI = Alt_yHeaderCheck(sectionheaderPI,section_to_check,"h4");
+          var sectionheaderPI = Alt_yHeaderCheck(sectionheaderPI,section_to_check,"h5");
+          var sectionheaderPI = Alt_yHeaderCheck(sectionheaderPI,section_to_check,"h6");
+          var sectionheaderPI = Alt_EmptyHeaderCheck(sectionheaderPI,section_to_check);
+
+          return sectionheaderPI;
+     }
+     function Alt_yHeaderCheck(sectionheaderPI, section_to_check, value_to_check){
+          if(typeof(sectionheaderPI) == 'undefined' || sectionheaderPI == 'null'){
+               var sectionheaderPI = section_to_check.getElementsByTagName(value_to_check)[0];
+          }
+          return sectionheaderPI;
+     }
+     function Alt_EmptyHeaderCheck(sectionheaderPI, section_to_check){
+          if(typeof(sectionheaderPI) == 'undefined' || sectionheaderPI == 'null'){
+               var sectionheaderPI = document.getElementsByClassName("voidvalue")[0];;
+          }
+          return sectionheaderPI;
+     }
+
+
+</script>
 
      function copyDivVO(){
 
